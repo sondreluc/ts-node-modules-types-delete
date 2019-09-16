364dacb50ba8f8ac1d0d7d3c11040a798e9555e3
Sondre Lucas Follesø <sondre@dogu.no>
2019-09-16 09:20:20 +0200

    adds more dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React, { useState, Fragment } from 'react';
 import { Button } from './Button';
 
 export const App = () => {
-  const [message, setMessage] = useState('Hello World!');
-  return (
-    <div>
-      <h1>{message}</h1>
-      <Button onClick={() => setMessage('Godbye!')} text="Change message" />
-    </div>
-  );
+    const [message, setMessage] = useState('Hello World!');
+    return (
+        <div>
+            <h1>{message}</h1>
+            <Button onClick={() => setMessage('Godbye!')} text="Change message" />
+        </div>
+    );
 };
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 type ButtonProps = {
-  onClick: React.MouseEventHandler<HTMLButtonElement>;
-  text: string;
+    onClick: React.MouseEventHandler<HTMLButtonElement>;
+    text: string;
 };
 
 export const Button = ({ onClick, text }: ButtonProps) => (
-  <button className="button" onClick={onClick}>
-    {text}
-  </button>
+    <button className="button" onClick={onClick}>
+        {text}
+    </button>
 );
